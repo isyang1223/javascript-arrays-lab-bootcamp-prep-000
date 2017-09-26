d103efccf5b81e120a619f9d92d66fdfc2fff706
isyang1223 <isyang1223@gmail.com>
2017-09-26 03:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
-function destructivelyRemoveLastKitten(name){
+function destructivelyRemoveLastKitten(){
   kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kiteens.shift()
 }
